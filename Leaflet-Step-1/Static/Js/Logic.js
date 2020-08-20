3a9c37e1baa9420a0cca16d12f4a1bb27342f7c2
manuelj22 <61300763+manuelj22@users.noreply.github.com>
2020-08-19 20:38:12 -0500

    .

--- Leaflet-Step-1/Static/Js/Logic.js
+++ Leaflet-Step-1/Static/Js/Logic.js
@@ -7,7 +7,8 @@ var myMap = L.map("map", {
   
   // We use the addTo method to add objects to our map
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
     maxZoom: 18,
     id: "mapbox.streets",
     accessToken: API_KEY
